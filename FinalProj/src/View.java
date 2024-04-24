59333e6ab6540dccdd88de766a2c7c3d8bbe667a
mebcool <160055684+mebcool@users.noreply.github.com>
2024-04-24 12:29:52 -0600

    Update View.java

--- FinalProj/src/View.java
+++ FinalProj/src/View.java
@@ -1,3 +1,5 @@
+package src;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
@@ -6,21 +8,25 @@ public class View {
 
     public JTextField inputUsernameField;
     public JTextField inputPasswordField;
-    public View(){
+    public View(//actionlistener
+    ){
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         JTabbedPane tabPanel = new JTabbedPane();
 
-        JPanel page1 = new JPanel(new BorderLayout());
+        JPanel page1 = new JPanel();
 
         inputUsernameField = new JTextField(15);
         inputPasswordField = new JTextField(15);
         JButton computeButton = new JButton("LOGIN");
 
-        page1.add(inputUsernameField, BorderLayout.NORTH);
-        page1.add(inputPasswordField, BorderLayout.CENTER);
-        page1.add(computeButton, BorderLayout.SOUTH);
+        page1.add(new JLabel("Username"));
+        page1.add(inputUsernameField);
+
+        page1.add(new JLabel("Password"));
+        page1.add(inputPasswordField);
+        page1.add(computeButton);
 
         tabPanel.addTab("Login", page1);
 
@@ -29,7 +35,7 @@ public class View {
         frame.add(tabPanel);
 
         frame.getContentPane().add(tabPanel);
-        frame.setSize(400, 300);
+        frame.setSize(350, 350);
         frame.setVisible(true);
     }
 }
